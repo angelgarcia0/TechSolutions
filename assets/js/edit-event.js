168ebcf0a9c2a8310aac466815267e15727caa0c
angelgarcia0 <garmiguelangel89@gmail.com>
2026-05-15 17:06:54 -0500

    feat: create edit academic event form

---
+++ assets/js/edit-event.js
@@ -0,0 +1,19 @@
+function updateEvent() {
+  const title = document.getElementById("title").value;
+  const date = document.getElementById("date").value;
+  const location = document.getElementById("location").value;
+  const description = document.getElementById("description").value;
+
+  const message = document.getElementById("message");
+
+  // ERROR INTENCIONAL
+  // Permite actualizar eventos sin título
+
+  if (date !== "" && location !== "" && description !== "") {
+    message.style.color = "green";
+    message.innerText = "Event updated successfully";
+  } else {
+    message.style.color = "red";
+    message.innerText = "All fields are required";
+  }
+}
