ba9dcf8841978bfdc323c6ab5cb21a96d33def9f
angelgarcia0 <garmiguelangel89@gmail.com>
2026-05-15 11:34:08 -0500

    .

--- assets/js/recover-password.js
+++ assets/js/recover-password.js
@@ -2,16 +2,14 @@ function recoverPassword() {
   const email = document.getElementById("email").value;
   const message = document.getElementById("message");
 
-  const emailRegex = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+  // ERROR INTENCIONAL
+  // Acepta cualquier texto como email
 
-  if (email === "") {
-    message.style.color = "red";
-    message.innerText = "Email is required";
-  } else if (!emailRegex.test(email)) {
-    message.style.color = "red";
-    message.innerText = "Invalid email format";
-  } else {
+  if (email !== "") {
     message.style.color = "green";
     message.innerText = "Recovery link sent successfully";
+  } else {
+    message.style.color = "red";
+    message.innerText = "Email is required";
   }
 }
