eb5cb7bdb4fdcfce36dfd11b7f87c37664a0ad8c
angelgarcia0 <garmiguelangel89@gmail.com>
2026-05-15 18:44:57 -0500

    hotfix: make event description optional

--- assets/js/create-event.js
+++ assets/js/create-event.js
@@ -1,14 +1,18 @@
 function createEvent() {
   const title = document.getElementById("title").value;
+
   const date = document.getElementById("date").value;
+
   const location = document.getElementById("location").value;
+
   const description = document.getElementById("description").value;
 
   const message = document.getElementById("message");
 
   const currentDate = new Date().toISOString().split("T")[0];
 
-  if (title !== "" && date !== "" && location !== "" && description !== "") {
+  // Descripción ahora es opcional
+  if (title !== "" && date !== "" && location !== "") {
     if (date >= currentDate) {
       message.style.color = "green";
       message.innerText = "Event created successfully";
@@ -18,6 +22,6 @@ function createEvent() {
     }
   } else {
     message.style.color = "red";
-    message.innerText = "All fields are required";
+    message.innerText = "Title, date and location are required";
   }
 }
