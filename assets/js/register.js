68913f15ec01c6769e48838882fc1ccbf8066949
angelgarcia0 <garmiguelangel89@gmail.com>
2026-05-15 10:53:48 -0500

    Password validation issue in register resolved

--- assets/js/register.js
+++ assets/js/register.js
@@ -6,17 +6,19 @@ function register() {
 
   const message = document.getElementById("message");
 
-  // ERROR INTENCIONAL
-  // Permite registrar aunque las contraseñas sean diferentes
-
   if (
     name !== "" &&
     email !== "" &&
     password !== "" &&
     confirmPassword !== ""
   ) {
-    message.style.color = "green";
-    message.innerText = "User registered successfully";
+    if (password === confirmPassword) {
+      message.style.color = "green";
+      message.innerText = "User registered successfully";
+    } else {
+      message.style.color = "red";
+      message.innerText = "Passwords do not match";
+    }
   } else {
     message.style.color = "red";
     message.innerText = "All fields are required";
