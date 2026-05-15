5d607f17a41044a775878f59ab46aa171bb43d94
angelgarcia0 <garmiguelangel89@gmail.com>
2026-05-15 18:05:54 -0500

    fix: validate register acceptance criteria

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,51 @@
-function login() {
-  const email = document.getElementById("email").value;
+function register() {
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
   const message = document.getElementById("message");
 
-  // Usuario simulado
-  const savedEmail = "admin@techsolutions.com";
-  const savedPassword = "123456";
+  // Simulación de correos registrados
+  const registeredEmails = ["admin@techsolutions.com", "user@gmail.com"];
 
-  // ERROR INTENCIONAL
-  if (email === savedEmail && password === savedPassword) {
-    message.innerText = "Login successful";
-  } else {
-    message.innerText = "Invalid credentials";
+  // Validar campos vacíos
+  if (
+    name === "" ||
+    email === "" ||
+    password === "" ||
+    confirmPassword === ""
+  ) {
+    message.style.color = "red";
+    message.innerText = "All fields are required";
+    return;
   }
+
+  // Validar longitud contraseña
+  if (password.length < 8) {
+    message.style.color = "red";
+    message.innerText = "Password must contain at least 8 characters";
+
+    return;
+  }
+
+  // Validar confirmación contraseña
+  if (password !== confirmPassword) {
+    message.style.color = "red";
+    message.innerText = "Passwords do not match";
+
+    return;
+  }
+
+  // Validar correo repetido
+  if (registeredEmails.includes(email)) {
+    message.style.color = "red";
+    message.innerText = "Email already registered";
+
+    return;
+  }
+
+  // Registro exitoso
+  message.style.color = "green";
+  message.innerText = "User registered successfully";
 }
